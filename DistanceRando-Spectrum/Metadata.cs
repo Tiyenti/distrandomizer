ad314efb692c7854d2ce49befa8b15193300328d
Reherc <amaury.vanoorenberghe@yahoo.com>
2021-07-18 02:27:45 +0200

    Added distandomizer button to the Arcade menu

--- DistanceRando-Spectrum/Metadata.cs
+++ DistanceRando-Spectrum/Metadata.cs
@@ -1,81 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace DistanceRando
 {
-    static class Metadata
-    {
-        public static readonly string RandomizerVersion = "A.2.1";
-        public static readonly string LogicVersion = "1.0-alpha2";
+	internal static class Metadata
+	{
+		public const string RandomizerVersion = "A.2.1";
+		public const string LogicVersion = "1.0-alpha2";
 
-        public static readonly Dictionary<char, string> FriendlyHashWords = new Dictionary<char, string>() {
-            { 'a', "Archaic"},
-            { 'b', "Boost"},
-            { 'c', "Catalyst"},
-            { 'd', "Diamond"},
-            { 'e', "Encryptor"},
-            { 'f', "Dropper"},
-            { 'g', "Grip"},
-            { 'h', "Archive"},
-            { 'i', "Interceptor"},
-            { 'j', "Jump"},
-            { 'k', "Monolith"},
-            { 'l', "Laser"},
-            { 'm', "Medal"},
-            { 'n', "Nitronic"},
-            { 'o', "Overheat"},
-            { 'p', "Checkpoint"},
-            { 'q', "Quarantine"},
-            { 'r', "Resonance"},
-            { 's', "Spectrum"},
-            { 't', "Teleporter"},
-            { 'u', "Corruption"},
-            { 'v', "Virus"},
-            { 'w', "Wings"},
-            { 'x', "CORE"},
-            { 'y', "Ascension"},
-            { 'z', "Zenith"},
-            { 'A', "Approach"},
-            { 'B', "Blocker"},
-            { 'C', "Compression"},
-            { 'D', "Diversion"},
-            { 'E', "Empire"},
-            { 'F', "Factory"},
-            { 'G', "Abyss"},
-            { 'H', "Halcyon"},
-            { 'I', "Inferno"},
-            { 'J', "Barrier"},
-            { 'K', "Knowledge"},
-            { 'L', "Research"},
-            { 'M', "Infection"},
-            { 'N', "Echoes"},
-            { 'O', "Observatory"},
-            { 'P', "Popup"},
-            { 'Q', "Affect"},
-            { 'R', "Roller"},
-            { 'S', "Saw"},
-            { 'T', "Entanglement"},
-            { 'U', "Utopia"},
-            { 'V', "Void"},
-            { 'W', "Shard"},
-            { 'X', "Sector"},
-            { 'Y', "Storm"},
-            { 'Z', "Perfect"},
-            { '0', "Terminus"},
-            { '1', "Adventure"},
-            { '2', "Skuttle"},
-            { '3', "Nexus"},
-            { '4', "Repulsion"},
-            { '5', "Euphoria"},
-            { '6', "Thrusters"},
-            { '7', "Rooftops"},
-            { '8', "Enemy"},
-            { '9', "Continuum"},
-            { '+', "Array"},
-            { '/', "Deterrence"}
-        };
+		public const string MenuButtonText = "Distandomizer";
+		public const string MenuButtonDesc = "Start a new randomized run of the official campaign";
 
-    }
+		public static readonly Dictionary<char, string> FriendlyHashWords = new Dictionary<char, string>() {
+			{ 'a', "Archaic"},
+			{ 'b', "Boost"},
+			{ 'c', "Catalyst"},
+			{ 'd', "Diamond"},
+			{ 'e', "Encryptor"},
+			{ 'f', "Dropper"},
+			{ 'g', "Grip"},
+			{ 'h', "Archive"},
+			{ 'i', "Interceptor"},
+			{ 'j', "Jump"},
+			{ 'k', "Monolith"},
+			{ 'l', "Laser"},
+			{ 'm', "Medal"},
+			{ 'n', "Nitronic"},
+			{ 'o', "Overheat"},
+			{ 'p', "Checkpoint"},
+			{ 'q', "Quarantine"},
+			{ 'r', "Resonance"},
+			{ 's', "Spectrum"},
+			{ 't', "Teleporter"},
+			{ 'u', "Corruption"},
+			{ 'v', "Virus"},
+			{ 'w', "Wings"},
+			{ 'x', "CORE"},
+			{ 'y', "Ascension"},
+			{ 'z', "Zenith"},
+			{ 'A', "Approach"},
+			{ 'B', "Blocker"},
+			{ 'C', "Compression"},
+			{ 'D', "Diversion"},
+			{ 'E', "Empire"},
+			{ 'F', "Factory"},
+			{ 'G', "Abyss"},
+			{ 'H', "Halcyon"},
+			{ 'I', "Inferno"},
+			{ 'J', "Barrier"},
+			{ 'K', "Knowledge"},
+			{ 'L', "Research"},
+			{ 'M', "Infection"},
+			{ 'N', "Echoes"},
+			{ 'O', "Observatory"},
+			{ 'P', "Popup"},
+			{ 'Q', "Affect"},
+			{ 'R', "Roller"},
+			{ 'S', "Saw"},
+			{ 'T', "Entanglement"},
+			{ 'U', "Utopia"},
+			{ 'V', "Void"},
+			{ 'W', "Shard"},
+			{ 'X', "Sector"},
+			{ 'Y', "Storm"},
+			{ 'Z', "Perfect"},
+			{ '0', "Terminus"},
+			{ '1', "Adventure"},
+			{ '2', "Skuttle"},
+			{ '3', "Nexus"},
+			{ '4', "Repulsion"},
+			{ '5', "Euphoria"},
+			{ '6', "Thrusters"},
+			{ '7', "Rooftops"},
+			{ '8', "Enemy"},
+			{ '9', "Continuum"},
+			{ '+', "Array"},
+			{ '/', "Deterrence"}
+		};
+
+	}
 }
